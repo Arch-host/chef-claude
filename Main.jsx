5d9e04a015a0fa8519ab665216898d0b70c00f8d
Arch-host <webneth99@gmail.com>
2025-10-28 09:48:08 +0100

    challenge

---
+++ Main.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+export default function Main() {
+
+    const [ingredients, setIgredients] = React.useState([]) /*check if igrediants.length >0 */
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIgredients(prevIngredient=> [...prevIngredient,newIngredient])
+    }
+
+    return(
+        <main>
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g Oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+            {ingredients.length > 0  ? <section> {/*if true displays this section... explic use of the > 0 */}
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>
+            </section> : null}
+        </main>
+    )
+}
